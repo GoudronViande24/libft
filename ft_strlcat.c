f69f84beab89a3f8436e2a7d195d20aaee208445
Thomas Fournier <thomas@artivain.com>
2023-05-03 16:48:21 -0400

    fix strlcat change most of how it's done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,21 +6,32 @@
 /*   By: thfourni <thfourni@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:19:10 by thfourni          #+#    #+#             */
-/*   Updated: 2023/02/21 16:14:55 by thfourni         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:46:50 by thfourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_strlcat(char *dest, char *src, int len)
 {
 	int	i;
+	int	dest_len;
+	int	src_len;
 
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (!len)
+		return (src_len);
+	if (len < dest_len)
+		src_len += len;
+	else
+		src_len += dest_len;
 	i = 0;
-	while (dest[i])
-		i++;
-	while (src[i] && i < len)
+	while (src[i] && (dest_len + i) < len - 1)
 	{
-		dest[i] = src[i];
+		dest[dest_len + i] = src[i];
 		i++;
 	}
-	return (i);
+	dest[dest_len + i] = 0;
+	return (src_len);
 }

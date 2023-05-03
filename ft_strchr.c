d79c598ce6c60ec1e494f354700a19c065347f2b
Thomas Fournier <thomas@artivain.com>
2023-05-03 17:04:03 -0400

    fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: thfourni <thfourni@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 02:33:41 by thfourni          #+#    #+#             */
-/*   Updated: 2023/05/03 16:56:38 by thfourni         ###   ########.fr       */
+/*   Updated: 2023/05/03 17:03:18 by thfourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ char	*ft_strchr(char *s, char c)
 			return (s + i);
 		i++;
 	}
+	if (s[i] == c)
+		return (s + i);
 	return (0);
 }

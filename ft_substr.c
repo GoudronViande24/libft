10cabf07b33ffe0e2d5bd2a2cb3d49d8ae41d8a4
Thomas Fournier <thomas@artivain.com>
2023-05-17 17:23:05 -0400

    improve substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: thfourni <thfourni@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 14:39:07 by thfourni          #+#    #+#             */
-/*   Updated: 2023/05/03 18:21:15 by thfourni         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:21:39 by thfourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ char	*ft_substr(const char *src, unsigned int start, unsigned int len)
 	char	*dest;
 
 	if (!src)
-		return (0);
+		return (NULL);
 	if ((unsigned int)ft_strlen(src) < start)
 		len = 0;
 	if ((unsigned int)ft_strlen(src + start) < len)
 		len = (unsigned int)ft_strlen(src + start);
 	dest = malloc(sizeof(char) * (len + 1));
 	if (!dest)
-		return (0);
+		return (NULL);
 	ft_strlcpy(dest, (char *)(src + start), len + 1);
 	return (dest);
 }

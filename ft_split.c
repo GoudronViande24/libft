c71ffa1a790182a5a8350138bfcccb4003350b34
Thomas Fournier <thomas@artivain.com>
2023-05-17 18:22:06 -0400

    finally, strict paco is happy

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: thfourni <thfourni@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 18:26:38 by thfourni          #+#    #+#             */
-/*   Updated: 2023/05/03 18:35:36 by thfourni         ###   ########.fr       */
+/*   Updated: 2023/05/17 18:20:29 by thfourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,30 +33,59 @@ static unsigned int	ft_countwords(const char *s, char c)
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+int	ft_flush(char **tab, int i)
 {
-	char			**tab;
-	unsigned int	i;
-	unsigned int	j;
+	while (i--)
+		free(tab[i]);
+	free(tab);
+	return (0);
+}
 
-	if (!s)
-		return (0);
+static void	ft_fill(char *dest, const char *src, char c)
+{
+	while (*src && *src != c)
+		*dest++ = *src++;
+	*dest = 0;
+}
+
+static int	ft_split_fill(char **tab, const char *s, char c)
+{
+	int	i;
+	int	k;
+
 	i = 0;
-	tab = malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
-	if (!tab)
-		return (0);
 	while (*s)
 	{
 		if (*s != c)
 		{
-			j = 0;
-			while (*s && *s != c && ++j)
-				s++;
-			tab[i++] = ft_substr(s - j, 0, j);
+			k = 0;
+			while (s[k] && s[k] != c)
+				k++;
+			tab[i] = malloc(sizeof(char) * (k + 1));
+			if (!tab[i])
+				return (ft_flush(tab, i));
+			ft_fill(tab[i++], s, c);
+			s += k;
 		}
 		else
 			s++;
 	}
-	tab[i] = 0;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	int		count;
+
+	if (!s)
+		return (NULL);
+	count = ft_countwords(s, c);
+	tab = malloc(sizeof(char *) * (count + 1));
+	if (!tab)
+		return (NULL);
+	tab[count] = NULL;
+	if (!ft_split_fill(tab, s, c))
+		return (NULL);
 	return (tab);
 }
